908cad760e37db2e30df12ee834e52487b6fc1ca
n3rdy <n3rdy@archtop.localdomain>
2021-06-18 23:21:17 +0530

    fixed for arch

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel] = { "#a89984", "#cc241d" },
+	[SchemeOut] = { "#a89984", "#689d6a" },
+	[SchemeSelHighlight] = { "#a89984", "#282828" },
+	[SchemeNormHighlight] = { "#a89984", "#fb4934" },
+
+};
--- config.h
+++ config.h
@@ -1,14 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+#include "colors-wal-dmenu.h"
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mononoki Nerd Font Mono:size=10"
+	"iosevka Nerd Font Mono:size=10"
 };
 
-
+/*
 static const char bg[] = "#282a36";
 static const char col_curline[] = "#44475a";
 static const char fg[] = "#f8f8f2";
@@ -20,17 +22,10 @@ static const char pink[] = "#ff79c6";
 static const char red[] = "#ff5555";
 static const char yellow[] = "#f1fa8c";
 static const char purple[] = "#bd93f9";
-
+*/
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { fg, bg },
-	[SchemeSel] = { purple, col_curline},
-	[SchemeSelHighlight] = { cyan, bg },
-	[SchemeNormHighlight] = { fg, comment },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+;
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
